34a812fa3a6ba19e157e2ea5408e7c555af2c67d
Ddor <115899390+ddor712@users.noreply.github.com>
2024-09-11 23:16:51 -0400

    Add files via upload
    This is the file with the code answering assignment 1

---
+++ assignment1/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Danny Doran
+// hello.c
+// This program outputs my name, my favorite color, and one thing im excited to do this semester
+
+int main(){
+
+    printf("NAME: Danny\n\n");
+
+    printf("COLOR: RED\n\n");
+
+    printf("QUEST: Excited to see what we learn this semester\n");
+
+    return 0;
+}
